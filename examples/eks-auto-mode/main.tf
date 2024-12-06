a1de32061985a783ced06dcd906dd1c1bdd2ef6e
ankurp-rafay <ankur@rafay.co>
2024-12-06 19:05:54 +0530

    Update main.tf

--- examples/eks-auto-mode/main.tf
+++ examples/eks-auto-mode/main.tf
@@ -83,10 +83,3 @@ module "vpc" {
   tags = local.tags
 }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_name
-}
