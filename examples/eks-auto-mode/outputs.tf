6331bf75b3b39ba3436e8445d792c6b9d4ab7eee
ankurp-rafay <ankur@rafay.co>
2024-12-06 18:40:55 +0530

    Update outputs.tf

--- examples/eks-auto-mode/outputs.tf
+++ examples/eks-auto-mode/outputs.tf
@@ -12,7 +12,6 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-
 output "cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
@@ -22,7 +21,6 @@ output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
   value       = module.eks.cluster_oidc_issuer_url
 }
-
 
 output "cluster_platform_version" {
   description = "Platform version for the cluster"
@@ -34,6 +32,26 @@ output "cluster_status" {
   value       = module.eks.cluster_status
 }
 
-
-
-
+output "kubeconfig" {
+  description = "Kubeconfig file content for the EKS cluster"
+  value       = <<EOT
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${module.eks.cluster_endpoint}
+    certificate-authority-data: ${data.aws_eks_cluster.eks.certificate_authority[0].data}
+  name: ${module.eks.cluster_name}
+contexts:
+- context:
+    cluster: ${module.eks.cluster_name}
+    user: ${module.eks.cluster_name}
+  name: ${module.eks.cluster_name}
+current-context: ${module.eks.cluster_name}
+kind: Config
+preferences: {}
+users:
+- name: ${module.eks.cluster_name}
+  user:
+    token: ${data.aws_eks_cluster_auth.eks.token}
+EOT
+}
