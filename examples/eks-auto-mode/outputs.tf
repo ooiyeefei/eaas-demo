961bf75771c148701d2db75caa1943056e066023
ankurp-rafay <ankur@rafay.co>
2024-12-07 08:30:43 +0530

    Update outputs.tf

--- examples/eks-auto-mode/outputs.tf
+++ examples/eks-auto-mode/outputs.tf
@@ -38,19 +38,19 @@ output "kubeconfig" {
 apiVersion: v1
 clusters:
 - cluster:
-    server: ${module.eks.cluster_endpoint}
-    certificate-authority-data: ${module.eks.cluster_certificate_authority_data}
-  name: ${module.eks.cluster_name}
+    server: "${module.eks.cluster_endpoint}"
+    certificate-authority-data: "${module.eks.cluster_certificate_authority_data}"
+  name: "${module.eks.cluster_name}"
 contexts:
 - context:
-    cluster: ${module.eks.cluster_name}
-    user: ${module.eks.cluster_name}
-  name: ${module.eks.cluster_name}
-current-context: ${module.eks.cluster_name}
+    cluster: "${module.eks.cluster_name}"
+    user: "${module.eks.cluster_name}"
+  name: "${module.eks.cluster_name}"
+current-context: "${module.eks.cluster_name}"
 kind: Config
 preferences: {}
 users:
-- name: ${module.eks.cluster_name}
+- name: "${module.eks.cluster_name}"
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1beta1
@@ -59,6 +59,6 @@ users:
       - eks
       - get-token
       - --cluster-name
-      - ${module.eks.cluster_name}
+      - "${module.eks.cluster_name}"
 EOT
 }
