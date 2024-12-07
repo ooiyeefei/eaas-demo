0d41a159eb8043c855927d48f1e6bc4e50cf8483
ankurp-rafay <ankur@rafay.co>
2024-12-07 09:27:09 +0530

    Update main.tf

--- examples/rafay-import/main.tf
+++ examples/rafay-import/main.tf
@@ -19,21 +19,21 @@ resource "null_resource" "download_kubectl" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-      wget "https://storage.googleapis.com/kubernetes-release/release/v1.28.2/bin/linux/amd64/kubectl" -O ./kubectl
+      wget -q "https://storage.googleapis.com/kubernetes-release/release/v1.28.2/bin/linux/amd64/kubectl" -O ./kubectl
       chmod +x ./kubectl
     EOT
   }
 }
 
+# Install jq binary if not present
 resource "null_resource" "install_jq" {
   provisioner "local-exec" {
     command = <<EOT
-      if ! command -v jq &> /dev/null
-      then
+      if ! command -v jq &> /dev/null; then
         echo "jq not found. Installing jq..."
         wget -q "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" -O jq
         chmod +x jq
-        sudo mv jq /usr/local/bin/
+        sudo mv jq /usr/local/bin/ || (chmod +x jq && mv jq ~/jq && export PATH=$PATH:~)
       fi
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -44,15 +44,20 @@ resource "null_resource" "install_jq" {
 resource "null_resource" "apply_bootstrap_yaml" {
   depends_on = [
     rafay_import_cluster.import_cluster,
-    null_resource.download_kubectl
+    null_resource.download_kubectl,
+    null_resource.install_jq
   ]
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-      # Extract raw kubeconfig value from the metadata
-      echo "${var.kubeconfig}" | jq -r '.value' > kubeconfig.yaml
-      
+      # Validate and extract raw kubeconfig value
+      if echo "${var.kubeconfig}" | jq -e '.value' > /dev/null; then
+        echo "${var.kubeconfig}" | jq -r '.value' > kubeconfig.yaml
+      else
+        echo "${var.kubeconfig}" > kubeconfig.yaml
+      fi
+
       # Apply the bootstrap YAML using kubectl
       ./kubectl --kubeconfig=kubeconfig.yaml apply -f - <<EOF
 ${rafay_import_cluster.import_cluster.bootstrap_data}
