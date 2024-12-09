00cc8ac57bbaf51e7ae010b4d7b13fce5155a34a
ankurp-rafay <ankur@rafay.co>
2024-12-09 17:48:51 +0530

    Update main.tf

--- examples/rafay-import/main.tf
+++ examples/rafay-import/main.tf
@@ -7,6 +7,17 @@ resource "local_file" "kubeconfig" {
   filename = local.kubeconfig_path
 }
 
+provider "aws" {
+    region = var.region
+}
+data "aws_eks_cluster" "cluster" {
+    name = var.cluster_name
+}
+data "aws_eks_cluster_auth" "ephemeral" {
+  name = var.cluster_name
+}
+
+
 resource "rafay_import_cluster" "import_cluster" {
   clustername           = var.cluster_name
   projectname           = var.project_name
@@ -24,9 +35,17 @@ resource "rafay_import_cluster" "import_cluster" {
   }
 }
 
+provider kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.ephemeral.token
+}
+
 provider "helm" {
   kubernetes {
-    config_path = local.kubeconfig_path
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.ephemeral.token
   }
 }
 
