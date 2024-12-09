4039b09d29ea730a6d6bb0db8b261126dc8f6c5e
ankurp-rafay <ankur@rafay.co>
2024-12-09 17:47:24 +0530

    Update variables.tf

--- examples/rafay-import/variables.tf
+++ examples/rafay-import/variables.tf
@@ -10,6 +10,11 @@ variable "kubeconfig_json" {
   default     = {}
 }
 
+variable "region" {
+  type        = string
+  default = "us-west-2"
+}
+
 variable "cluster_name" {
   type        = string
   description = "Name of the cluster"
