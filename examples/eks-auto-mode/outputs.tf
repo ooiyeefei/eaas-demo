45c00fea15d406b77217c36d6c023402345a247d
ankurp-rafay <ankur@rafay.co>
2024-12-09 16:04:28 +0530

    Update outputs.tf

--- examples/eks-auto-mode/outputs.tf
+++ examples/eks-auto-mode/outputs.tf
@@ -40,24 +40,29 @@ clusters:
 - cluster:
     server: "${module.eks.cluster_endpoint}"
     certificate-authority-data: "${module.eks.cluster_certificate_authority_data}"
-  name: "${module.eks.cluster_name}"
+  name: "${module.eks.cluster_arn}"
 contexts:
 - context:
-    cluster: "${module.eks.cluster_name}"
-    user: "${module.eks.cluster_name}"
-  name: "${module.eks.cluster_name}"
-current-context: "${module.eks.cluster_name}"
+    cluster: "${module.eks.cluster_arn}"
+    user: "${module.eks.cluster_arn}"
+  name: "${module.eks.cluster_arn}"
+current-context: "${module.eks.cluster_arn}"
 kind: Config
 preferences: {}
 users:
-- name: "${module.eks.cluster_name}"
+- name: "${module.eks.cluster_arn}"
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1beta1
-      command: "./aws-iam-authenticator"
       args:
-      - token
-      - -i
+      - --region
+      - "${module.eks.region}"
+      - eks
+      - get-token
+      - --cluster-name
       - "${module.eks.cluster_name}"
+      - --output
+      - json
+      command: aws
 EOT
 }
