82aa38d27641c2b6d04cb52fa69022d0b3c358ee
ankurp-rafay <ankur@rafay.co>
2025-01-20 14:10:15 +0530

    fix path

--- tf-code/cmd-exec/main.tf
+++ tf-code/cmd-exec/main.tf
@@ -1,5 +1,5 @@
 module "rafay_cmdexec" {
-  source        = "./modules/rafay_cmdexec"
+  source        = "../../modules/rafay_cmdexec"
   api_key       = var.api_key
   project_name  = var.project_name
   cluster_name  = var.cluster_name
