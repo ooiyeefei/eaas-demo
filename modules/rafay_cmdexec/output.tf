22686d4275517a5269973f0499d54de38df095cd
ankurp-rafay <ankur@rafay.co>
2025-01-21 11:19:09 +0530

    Update output.tf

--- modules/rafay_cmdexec/output.tf
+++ modules/rafay_cmdexec/output.tf
@@ -1,3 +1,3 @@
-output "command_result" {
-  value = jsondecode(data.local_file.command_output.content)["command_output"]
+output "command_output" {
+  value = data.local_file.command_output.content
 }
