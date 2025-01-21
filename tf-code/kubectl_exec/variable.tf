eec81e68eeec8a31c6be22ca24672db8a2d258a5
ankurp-rafay <ankur@rafay.co>
2025-01-21 08:53:26 +0530

    Update variable.tf

--- tf-code/kubectl_exec/variable.tf
+++ tf-code/kubectl_exec/variable.tf
@@ -2,3 +2,8 @@ variable "cluster_name" {
   description = "The name of the Kubernetes cluster"
   type        = string
 }
+
+variable "project_name" {
+  type        = string
+  description = "Name of the project"
+}
