859baee265ff19dfaed53c0dd492e64fd5e40c92
ooiyeefei <yeefeiooi@gmail.com>
2025-06-25 14:51:20 +0800

    Update provider.tf

--- tf-code/rafay-import/provider.tf
+++ tf-code/rafay-import/provider.tf
@@ -1,8 +1,17 @@
 terraform {
   required_providers {
     rafay = {
-      version = ">= 1.0"
+      version = "= 1.1.47"
       source  = "RafaySystems/rafay"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.16.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
     }
   }
 }
