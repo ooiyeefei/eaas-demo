0d45f88eb5556e32142aca63d8d1f8eb1b55cd52
ooiyeefei <yeefeiooi@gmail.com>
2025-07-06 12:55:10 +0800

    Update delete-webhook.sh to be dynamic with amd and arm architecture based on provisioned infra

--- tf-code/rafay-import/delete-webhook.sh
+++ tf-code/rafay-import/delete-webhook.sh
@@ -10,11 +10,18 @@
 TMP_DIR=$(mktemp -d)
 cd $TMP_DIR
 
-wget https://rafay-prod-cli.s3-us-west-2.amazonaws.com/publish/rctl-linux-amd64.tar.bz2
-tar -xf rctl-linux-amd64.tar.bz2
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH="amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+  ARCH="arm64"
+fi
+
+wget "https://rafay-prod-cli.s3-us-west-2.amazonaws.com/publish/rctl-linux-${ARCH}.tar.bz2"
+tar -xf "rctl-linux-${ARCH}.tar.bz2"
 ./rctl download kubeconfig --cluster ${CLUSTER_NAME} -p ${PROJECT} > ztka-user-kubeconfig
 export KUBECONFIG=ztka-user-kubeconfig
-wget https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl
+wget "https://dl.k8s.io/release/v1.31.0/bin/linux/${ARCH}/kubectl"
 chmod +x kubectl
 ./kubectl delete validatingwebhookconfiguration rafay-drift-validate-v3
 
